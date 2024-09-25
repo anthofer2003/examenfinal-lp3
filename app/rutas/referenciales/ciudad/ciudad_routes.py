2da9b0f016a351b4982d2896886bcfbde042748c
Anthony Fernandez <fernandezanthony260@gmail.com>
2024-09-25 13:09:46 -0400

    fix: formulario innecesario de ciudad

--- app/rutas/referenciales/ciudad/ciudad_routes.py
+++ app/rutas/referenciales/ciudad/ciudad_routes.py
@@ -1,19 +1,8 @@
-from flask import Blueprint, render_template, jsonify
-from app.dao.referenciales.ciudad.CiudadDao import CiudadDao
+from flask import Blueprint, render_template
 
 ciumod = Blueprint('ciudad', __name__, template_folder='templates')
 
 @ciumod.route('/ciudad-index')
 def ciudadIndex():
-    ciudao = CiudadDao()
-    return render_template('ciudad-index.html', lista_ciudades=ciudao.getCiudades())
-
-@ciumod.route('/ciudad-agregar')
-def ciudadAgregar():
-    return render_template('ciudad-agregar.html')
-
-@ciumod.route('/get-ciudades')
-def getCiudades():
-    ciudao = CiudadDao()
-    return jsonify(ciudao.getCiudades())
+    return render_template('ciudad-index.html')
 
