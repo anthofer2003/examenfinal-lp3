374d4cc2e79e0f489d82b3d6ad4d63eb813e8222
Anthony Fernandez <fernandezanthony260@gmail.com>
2024-10-30 14:11:42 -0300

    feat: agregando las rutas de pedido de compras

--- app/__init__.py
+++ app/__init__.py
@@ -45,7 +45,18 @@ app.register_blueprint(examod, url_prefix=f'{modulo0}/examen')
 app.register_blueprint(certimod, url_prefix=f'{modulo0}/certificado')
 app.register_blueprint(turmod, url_prefix=f'{modulo0}/turno')
 
+# importar gestionar compras
+from app.rutas.gestionar_compras.registrar_pedido_compras.registrar_pedido_compras_routes \
+    import pdcmod
+
+# registro de modulos - gestionar compras
+modulo1 = '/gestionar-compras'
+app.register_blueprint(pdcmod, url_prefix=f'{modulo1}/registrar-pedido-compras')
+
 from app.rutas.referenciales.ciudad.ciudad_api import ciuapi
+from app.rutas.referenciales.sucursal.sucursal_api import sucapi
+from app.rutas.gestionar_compras.registrar_pedido_compras.registrar_pedido_compras_api \
+    import pdcapi
 from app.rutas.referenciales.pais.pais_api import paisapi
 from app.rutas.referenciales.nacionalidad.nacionalidad_api import nacioapi
 from app.rutas.referenciales.cargo.cargo_api import carapi
@@ -66,23 +77,27 @@ from app.rutas.referenciales.certificado.certificado_api import tipocertificado_
 from app.rutas.referenciales.turno.turno_api import turno_api
 
 # APIS v1
-version1 = '/api/v1'
-app.register_blueprint(ciuapi, url_prefix=version1)
-app.register_blueprint(paisapi, url_prefix=version1)
-app.register_blueprint(nacioapi, url_prefix=version1)
-app.register_blueprint(carapi, url_prefix=version1)
-app.register_blueprint(perapi, url_prefix=version1)
-app.register_blueprint(estudiante_api, url_prefix=version1)
-app.register_blueprint(programa_api, url_prefix=version1)
-app.register_blueprint(profesorapi, url_prefix=version1)
-app.register_blueprint(matricula_api, url_prefix=version1)
-app.register_blueprint(notaapi, url_prefix=version1)
-app.register_blueprint(horarios_api, url_prefix=version1)
-app.register_blueprint(evaluacion_api, url_prefix=version1)
-app.register_blueprint(asignatura_api, url_prefix=version1)
-app.register_blueprint(cursos_api, url_prefix=version1)
-app.register_blueprint(facultad_api, url_prefix=version1)
-app.register_blueprint(carrera_api, url_prefix=version1)
-app.register_blueprint(tipo_examen_api, url_prefix=version1)
-app.register_blueprint(tipocertificado_api, url_prefix=version1)
-app.register_blueprint(turno_api, url_prefix=version1)
+apiversion1 = '/api/v1'
+app.register_blueprint(ciuapi, url_prefix=apiversion1)
+app.register_blueprint(sucapi, url_prefix=apiversion1)
+app.register_blueprint(paisapi, url_prefix=apiversion1)
+app.register_blueprint(nacioapi, url_prefix=apiversion1)
+app.register_blueprint(carapi, url_prefix=apiversion1)
+app.register_blueprint(perapi, url_prefix=apiversion1)
+app.register_blueprint(estudiante_api, url_prefix=apiversion1)
+app.register_blueprint(programa_api, url_prefix=apiversion1)
+app.register_blueprint(profesorapi, url_prefix=apiversion1)
+app.register_blueprint(matricula_api, url_prefix=apiversion1)
+app.register_blueprint(notaapi, url_prefix=apiversion1)
+app.register_blueprint(horarios_api, url_prefix=apiversion1)
+app.register_blueprint(evaluacion_api, url_prefix=apiversion1)
+app.register_blueprint(asignatura_api, url_prefix=apiversion1)
+app.register_blueprint(cursos_api, url_prefix=apiversion1)
+app.register_blueprint(facultad_api, url_prefix=apiversion1)
+app.register_blueprint(carrera_api, url_prefix=apiversion1)
+app.register_blueprint(tipo_examen_api, url_prefix=apiversion1)
+app.register_blueprint(tipocertificado_api, url_prefix=apiversion1)
+app.register_blueprint(turno_api, url_prefix=apiversion1)
+
+# Gestionar compras API
+app.register_blueprint(pdcapi, url_prefix=f'{apiversion1}/{modulo1}/registrar-pedido-compras')
